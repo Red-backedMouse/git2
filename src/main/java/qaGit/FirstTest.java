28b4d64108147689cf9c54537877c9a990d4d89b
Ekaterina <ekormashova@gmail.com>
2023-01-31 23:11:21 +0300

    first test

---
+++ src/main/java/qaGit/FirstTest.java
@@ -0,0 +1,12 @@
+package qaGit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstTest {
+    @Test
+    public void firstTest(){
+        assertTrue(true);
+    }
+}
